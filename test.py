c8bd176e55a837aa01f006ce4ad19cef0fbf10ca
Tony Beeman <abeeman@microsoft.com>
2024-05-24 16:09:14 -0700

    Update tests

--- test.py
+++ test.py
@@ -1,53 +1,8 @@
 import promptgen
 import argparse
-
-g_test_prompts = {
-    "basic":
-    {
-        "batch_count": 3,
-        "batch_size": 4,
-        "spans": [
-        r'iteration',
-        r'update_c(pad(rndi(0,100), 3), 2)',
-        r'nexta(rngi(1,5))',
-        r'update_b(nexta(list("test_list")))',
-        r'foreach(["a", "b", "c", "d"], 2)'
-        ],
-    },
-    "vars": {
-        "batch_count": 2,
-        "batch_size": 4,
-        "spans": [
-            r'iteration',
-            r'update_c(save_var("sv-test", pad(rndi(0,100), 3)), 2)',
-            r'var("sv-test")',
-            r'var("test_var_255")'
-        ]
-    },
-    "math": {
-        "batch_size": 1,
-        "batch_count": 1,
-        "spans":
-            [
-            r'1 + 1',
-            r'3 - 4',
-            r'10 / 2',
-            r'2 * 2',
-            r'13 % 5'
-            ]
-    },
-    "precedence": {
-        "batch_size": 1,
-        "batch_count": 1,
-        "spans": [
-            r'10 / 2 + 3',
-            r'3 + 10 / 2',
-            r'(10 / 2) + 3',
-            r'10 / (2 + 3)',
-            r'10 / -(2 + 3)'
-        ]
-    }
-}
+import difflib
+import json
+import unittest
 
 
 class TestHandler(promptgen.CallbackHandler):
@@ -84,14 +39,20 @@ class TestHandler(promptgen.CallbackHandler):
                 return 255
         return None
 
-def print_prompts(prompts):
-    for i in range(0, len(prompts)):
-        print(f"{str(i):>04}: {prompts[i]}")
+def combine_strings(prompts):
+    return "\n".join(prompts)
 
-def basic_tests(debug=False):
-    handler = TestHandler()       
-    for test_name in g_test_prompts.keys():
-        test = g_test_prompts[test_name]
+
+def basic_tests(debug=False, output_tests=False):
+    with open('test.json') as test_file:
+        file_contents = test_file.read()
+    
+    test_prompts = json.loads(file_contents)
+
+    handler = TestHandler()
+    errors = ""
+    for test_name in test_prompts.keys():
+        test = test_prompts[test_name]
         template = ''
         print(f"Running {test_name}")
         for c in test["spans"]:
@@ -100,22 +61,43 @@ def basic_tests(debug=False):
             print(promptgen.debug_template(template, callback_handler=handler))
         else:
             prompts = promptgen.generate_prompts(template, batch_count=test["batch_count"], batch_size=test["batch_size"], callback_handler=handler)
-            print_prompts(prompts)
-            
+            actual = combine_strings(prompts)
+            if (output_tests):
+                test_prompts[test_name]["expected"] = actual
+            else:
+                # print(actual)
+                # print("DIFF")
+                expected = test["expected"]
+                if (expected == actual):
+                    print(f"{test_name} test succeeded.")
+                else:
+                    print(f"{test_name} failed:")
+                    d = difflib.Differ()
+                    diffs = list(difflib.unified_diff(actual.splitlines(keepends=True), test["expected"].splitlines(keepends=True)))
+                    dstr = '\n'.join(diffs) # "!" + diffs[0] + "!" #''.join(d.compare(actual, test["expected"]))
+                    print(dstr)
+                
+    if (output_tests):
+        with open('test_output.json', 'w') as out_file:
+            json.dump(test_prompts, out_file, indent=4)
+        print("test_output.json written to disk.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--debug", help="debug the test prompts", action="store_true")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("-st", "--save_tests", help="Outputs the contents of the basic tests with expected results.", action="store_true")
     parser.add_argument("-t", "--template", type=str, help="test a custom template")
     parser.add_argument("-bs", "--batch_size", type=int, default = 1, help="Provide a custom batch_size when using -t")
     parser.add_argument("-bc", "--batch_count", type=int, default = 1, help="Provide a custom batch_count when using -t")
     args = parser.parse_args()
     debug = True if args.debug else False
-    if (args.template):
+    if (args.save_tests):
+        basic_tests(debug=False, output_tests=True)
+    elif (args.template):
         if (debug):
             print(promptgen.debug_template(args.template))
         else:
-            print_prompts(promptgen.generate_prompts(args.template, args.batch_count, args.batch_size))
+            print(combine_strings(promptgen.generate_prompts(args.template, args.batch_count, args.batch_size)))
     else:
         basic_tests(debug)
